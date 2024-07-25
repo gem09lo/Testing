1431e553f0cb68b281a6fcb4e6c1e16143e53005
gem09lo <166319232+gem09lo@users.noreply.github.com>
2024-07-25 17:59:25 +0100

    Asked user for favourite colour

--- Testing.py
+++ Testing.py
@@ -5,3 +5,6 @@ print("Hello this is a testing file")
 name = input("What is your name?")
 print("Hello {}".format(name))
 
+colour = input("What is your favourite colour?")
+print("{} is my favourite too!".format(colour))
+
